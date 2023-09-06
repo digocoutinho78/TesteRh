5cb78669275218ca6feb4caf494f9cfc586beb6a
Rodrigo <digocoutinho@outlook.com>
2023-09-06 11:57:22 -0300

    insercao  dados func1

--- testerh/src/main/java/com/testerh/testerh/Funcionario.java
+++ testerh/src/main/java/com/testerh/testerh/Funcionario.java
@@ -6,8 +6,14 @@ import com.testerh.testerh.Enum.Funcao;
 
 public abstract class Funcionario extends Pessoa {
 
-    public Funcionario(int id, String nome, String cpf, String endereco) {
+    public Funcionario(int id, String nome, String cpf, String endereco, String matricula, Funcao funcao) {
         super(id, nome, cpf, endereco);
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.matricula = matricula;
+        this.funcao = funcao;
 
     }
 
--- testerh/src/main/java/com/testerh/testerh/TesterhApplication.java
+++ testerh/src/main/java/com/testerh/testerh/TesterhApplication.java
@@ -5,12 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.testerh.testerh.Funcionario;
 import com.testerh.testerh.Pessoa;
+import com.testerh.testerh.Enum.Funcao;
 
 @SpringBootApplication
 public class TesterhApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesterhApplication.class, args);
+
+		Funcionario func1 = new Funcionario(01, "Rodrigo Coutinho", "1234567921", "Rua dos bobos,00", "101010",
+				Funcao.ATENDENTE) {
+
+		};
 
 	}
 
